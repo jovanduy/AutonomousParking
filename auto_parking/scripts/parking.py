bf93e6a32c3d76010fdbb4091b71929756a4e841
Kim Asenbeck <kasenbec@wellesley.edu>
2017-04-23 17:46:02 -0400

    made some changes after doing code smell review. Please don't merge these changes until we have a chance to test this on a Neato! Feel free to comment though.

--- auto_parking/scripts/parking.py
+++ auto_parking/scripts/parking.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-""" This is a script that autonomously parks the Neato."""
+""" This is a script that autonomously parks the Neato either in parallel or perpendicular mode."""
 
 import rospy
 import numpy as np
@@ -12,7 +12,7 @@ import math
 LENGTH_OF_SPOT = 0.5 # The parking spots are half a meter long.
 STOP = Twist(linear=Vector3(0,0,0), angular=Vector3(0,0,0))
 SPEED = 0.2
-FORWARD_SLOW = Twist(linear=Vector3(SPEED,0.0,0.0), angular=Vector3(0.0,0.0,0.0))
+FORWARD = Twist(linear=Vector3(SPEED,0.0,0.0), angular=Vector3(0.0,0.0,0.0))
 
 
 class ParkingNode(object):
@@ -34,24 +34,25 @@ class ParkingNode(object):
         
     def process_scan(self, m):
         """ This function is the callback for our laser subscriber. """
-        self.ranges = m.ranges
+        currDist = m.ranges[270]
         if self.timestamp2 is None:
-            self.twist = FORWARD_SLOW
+            self.twist = FORWARD
             if self.dist2Neato is None:
-                self.dist2Neato = self.ranges[270]
+                self.dist2Neato = currDist
                 self.adjustment = 0.2 if self.is_parallel else -0.1
-                print self.dist2Neato
-            elif self.ranges[270] > (self.dist2Neato + LENGTH_OF_SPOT - .05):
-                self.dist2Wall = self.ranges[270]
+                print "dist2Neato: ", self.dist2Neato
+            elif currDist > (self.dist2Neato + LENGTH_OF_SPOT - .05):
+                self.dist2Wall = currDist
                 self.radius = (self.dist2Wall/2.0)
                 if self.timestamp1 is None:
                     self.timestamp1 = rospy.Time.now()
                     print "TIME1: ", self.timestamp1
-            if abs(self.ranges[270] - self.dist2Neato) <= 0.2 and self.timestamp1 is not None:
+            if abs(currDist - self.dist2Neato) <= 0.2 and self.timestamp1 is not None:
                 self.timestamp2 = rospy.Time.now()
                 print "TIME2: ",  self.timestamp2
                 self.twist = STOP
 
+        # If we have both timestamps, we can determine the width of the spot, and begin parking. 
         elif self.timestamp1 is not None and self.timestamp2 is not None:
             self.widthOfSpot = SPEED * (self.timestamp2.secs - self.timestamp1.secs)
             if self.dist2Neato >= 0.3 and not self.isAligned: #determine later what the threshold should be
@@ -60,8 +61,7 @@ class ParkingNode(object):
                 rospy.signal_shutdown("Done parking.")
             elif self.dist2Neato < 0.3:
                 print "Neato was too close to park!"
-            
-          
+               
        
     def stop(self):
         """This method publishes a twist to make the Neato stop."""
@@ -74,52 +74,57 @@ class ParkingNode(object):
         now = rospy.Time.now()
         travelTime = dist/SPEED #dist/speed = time
         while rospy.Time.now() - now <= rospy.Duration(travelTime):
-            self.twist = FORWARD_SLOW
+            self.twist = FORWARD
         self.twist = STOP
         self.isAligned = True
 
-    def drive_arc(self, omega, travelTime, forward=False):
+        
+    def drive_arc(self, omega, travelTime, sign):
+        # The third parameter (sign) represents whether the forward velocity of the twist will be positive or negative
         if forward:
             now = rospy.Time.now()
             while rospy.Time.now() - now <= travelTime:
-                self.twist = Twist(linear=Vector3(SPEED,0,0), angular=Vector3(0,0,omega))
-        else:
-            now = rospy.Time.now()
-            while rospy.Time.now() - now <= travelTime:
-                self.twist = Twist(linear=Vector3(-SPEED,0,0), angular=Vector3(0,0,omega))
+                self.twist = Twist(linear=Vector3(sign*SPEED,0,0), angular=Vector3(0,0,omega))
 
+                
     def park(self):
         if self.is_parallel:
             # first arc
             omega = SPEED / (self.radius + 0.25)
             travelTime = rospy.Duration(math.pi/2.5/omega)
-            drive_arc(omega, travelTime)
+            drive_arc(omega, travelTime, -1)
+            
             # second arc
             omega = -SPEED/self.radius
             travelTime = rospy.Duration(math.pi/3.0/omega - 0.2)
-            drive_arc(omega, travelTime)
+            drive_arc(omega, travelTime, -1)
+            
             # drive forward to realign
             omega = -0.4
             travelTime = rospy.Duration(1)
-            drive_arc(omega, travelTime, True)
+            drive_arc(omega, travelTime, 1)
+            
         else:
+            # drive into spot
             omega = SPEED / (self.radius + 0.15)
             travelTime = rospy.Duration(math.pi/2.0/omega)
-            # drive into spot
-            drive_arc(omega, travelTime)
+            drive_arc(omega, travelTime, -1)
+            
             # drive back to fully enter spot
-            drive_arc(0, rospy.Duration(1))
+            drive_arc(0, rospy.Duration(1), -1)
+            
         self.twist = STOP
 
+        
     def run(self):
         """ This function is the main run loop."""
         rospy.on_shutdown(self.stop)
-       
         while not rospy.is_shutdown():
             if self.twist:
                 self.publisher.publish(self.twist)
             self.r.sleep()
-        
+
+            
 if __name__ == '__main__':            
     rospy.init_node('parking')
     parking_node = ParkingNode()
